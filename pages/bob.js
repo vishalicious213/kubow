dd93ac6af3ad494f12f5da47cb54903891849460
vishalicious213 <vishalicious213@gmail.com>
2022-01-30 19:39:02 -0800

    solved bob.js challenge

--- pages/bob.js
+++ pages/bob.js
@@ -1,9 +1,11 @@
 // Functions
 
 const bob = document.querySelector('.face')
+let margin = 100
 
 function moveBob() {
-
+    bob.style.marginLeft = `${margin}px`
+    margin = margin + 50
 }
 
 bob.addEventListener('click', moveBob)
