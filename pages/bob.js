884d7fcb8f2032737ff077248ab637fac4f406ab
vishalicious213 <vishalicious213@gmail.com>
2022-01-30 19:41:58 -0800

    solved bob.js challenge - added notes

--- pages/bob.js
+++ pages/bob.js
@@ -9,3 +9,9 @@ function moveBob() {
 }
 
 bob.addEventListener('click', moveBob)
+
+// Had to make it so that when Bob is clicked, it moves to the right.
+// Added function that modifies CSS to add 50 to left-margin on click.
+
+// Used this as a guide:
+// document.getElementById('someID').style.backgroundColor = "blue";
