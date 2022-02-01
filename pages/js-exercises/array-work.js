39d18b4491a234affc1a18c262380a6d3e5bbfd2
vishalicious213 <vishalicious213@gmail.com>
2022-01-31 18:31:42 -0800

    array-work: solved getCelcius

--- pages/js-exercises/array-work.js
+++ pages/js-exercises/array-work.js
@@ -1,8 +1,15 @@
 // Array work!
 
-// Using 'map' write a function that converts farenheit to celcius.
+// Using 'map' write a function that converts fahrenheit to celcius.
 // eg: getCelcius([12, 140, 212, 41]) => [-5, 60, 100, 5]
 
-function getCelcius() {
-    
+function getCelcius(fah) {
+    let cel = (fah - 32) / 1.8          // do the math
+    cel = cel.toFixed(2)                // limit the decimals (converts to string)
+    console.log(`${fah}F = ${cel}C`)
 }
+
+getCelcius(12)
+getCelcius(140)
+getCelcius(212)
+getCelcius(41)
