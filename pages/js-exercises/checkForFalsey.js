fef6f78da3218e6c841569b80606789f937dc10d
vishalicious213 <vishalicious213@gmail.com>
2022-02-04 15:13:16 -0800

    updated js-exercises/checkForFalsey.js

--- pages/js-exercises/checkForFalsey.js
+++ pages/js-exercises/checkForFalsey.js
@@ -1,10 +1,23 @@
 // Using `some` write a function that checks an array for a `falsey` value.
 // eg: checkForFalsey([11, Nan, [], 'Angels']) => true
 
-let values = [11, NaN, [], 'Angels']
+let values = [11, NaN, [], 'Angels'] // NaN is falsey
+// let values = [11, 1, [], 'Angels'] // removed NaN to test
+
+// FIRST SOLUTION
+
+// function checkForFalsey(arr) {
+//     return arr.some(value => value == false)
+// }
+
+// console.log(checkForFalsey(values))
+// // true
+
+
+// SECOND SOLUTION
 
 function checkForFalsey(arr) {
-    return arr.some(value => value == false)
+    return arr.some(value => !value)
 }
 
 console.log(checkForFalsey(values))
