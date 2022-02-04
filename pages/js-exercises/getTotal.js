a58a7ff0aa6fe2ff63aaa00d2b40f0aff0e97efa
vishalicious213 <vishalicious213@gmail.com>
2022-02-04 15:40:54 -0800

    updated js-exercises/getTotal.js

--- pages/js-exercises/getTotal.js
+++ pages/js-exercises/getTotal.js
@@ -1,3 +1,10 @@
 // Using `reduce` write a function that will return the total number of characters in an array of strings.
 // eg: getTotal(['Rabbit', 'Football', 'Cracking']) => 22
 
+const words = ['Rabbit', 'Football', 'Cracking']
+
+function getTotal(arr) {
+    return arr.reduce((total, word) => total + word.length, 0)
+}
+
+console.log(getTotal(words))
