a7e1a6aff40c0e4ad178c844b38acb74b1990c97
vishalicious213 <vishalicious213@gmail.com>
2022-02-16 15:35:06 -0800

    drafted js-exercises/getWords.js

---
+++ pages/js-exercises/getWords.js
@@ -0,0 +1,9 @@
+// Using an array method, write a function that returns the string elements of an array that have a given length or larger.
+// eg: getWords(['Florida', 'dog', 'phone', 5]) => ['Florida', 'phone']
+
+const wordsArray = ['Florida', 'dog', 'phone']
+const number = 5
+
+function getWords() {
+    
+}
