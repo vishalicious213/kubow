25f0f8252695d6876658db6f66881b877c020070
vishalicious213 <vishalicious213@gmail.com>
2022-02-16 19:11:37 -0800

    updated js-exercises/getValues.js

--- pages/js-exercises/getValues.js
+++ pages/js-exercises/getValues.js
@@ -1,6 +1,14 @@
-// Using an array method, write a function that converts an array of cm v alues as strings into an array of numbers
+// Using an array method, write a function that converts an array of cm values as strings into an array of numbers
 // eg: getValues(['23cm', '5.6cm', '1000cm']) => [23, 5.6, 1000]
 
-function getValues() {
-    
+const numsCM = ['23cm', '5.6cm', '1000cm']
+
+function getValues(nums) {
+    return nums.map(num => parseFloat(num))
 }
+
+console.log(getValues(numsCM))
+
+
+// https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l
+// Used #3, parseFloat()
