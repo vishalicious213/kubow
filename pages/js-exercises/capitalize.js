5db05a9fbd32543f63d0af5a66e064a9f3908ab9
vishalicious213 <vishalicious213@gmail.com>
2022-02-16 19:49:33 -0800

    drafted js-exercises/capitalize.js

---
+++ pages/js-exercises/capitalize.js
@@ -0,0 +1,6 @@
+// Using 'split', 'map' and 'join' write a function that capitalizes the first letter of each word in a sentence
+// eg: capitalize('the queens gambit') => ['The Queens Gambit']
+
+function capitalize() {
+    
+}
