371216cc748ee0a335782ed7505dd1b520b74e44
vishalicious213 <vishalicious213@gmail.com>
2022-02-16 17:53:40 -0800

    drafted js-exercises/getValues.js

---
+++ pages/js-exercises/getValues.js
@@ -0,0 +1,6 @@
+// Using an array method, write a function that converts an array of cm v alues as strings into an array of numbers
+// eg: getValues(['23cm', '5.6cm', '1000cm']) => [23, 5.6, 1000]
+
+function getValues() {
+    
+}
