29283b980ee4df264fd8882823cab06784a0399a
vishalicious213 <vishalicious213@gmail.com>
2022-02-16 19:41:42 -0800

    updated js-exercises/getVowelCount.js

--- pages/js-exercises/getVowelCount.js
+++ pages/js-exercises/getVowelCount.js
@@ -1,6 +1,17 @@
 // Using 'split' and 'filter' write a function that counts the number of vowels in a sentence.
 // eg: getVowelCount('In West Philadephia, born and raised') => 12
 
-function getVowelCount() {
-    
+const sampleSentence = 'In West Philadephia, born and raised'
+const vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+function getVowelCount(sentence) {
+    return sentence.split('').filter(letter => vowels.includes(letter))
 }
+
+console.log(getVowelCount(sampleSentence))
+
+
+// UPER
+// split string into individual letters using empty separator
+// filter out the vowels
+// count vowels & return count
