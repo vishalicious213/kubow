da1bae7a827bd327de8b7eacfcb87528d148db03
vishalicious213 <vishalicious213@gmail.com>
2022-02-16 19:18:55 -0800

    drafted js-exercises/getVowelCount.js

---
+++ pages/js-exercises/getVowelCount.js
@@ -0,0 +1,6 @@
+// Using 'split' and 'filter' write a function that counts the number of vowels in a sentence.
+// eg: getVowelCount('In West Philadephia, born and raised') => 12
+
+function getVowelCount() {
+    
+}
